b44663ca50f49cbf7858bf4041fee9cd601962d8
YsraelBaruk <81303434+YsraelBaruk@users.noreply.github.com>
2022-09-18 19:15:51 -0300

    app pronto

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import TelaJavaScript from "./componentes/TelaJavaScript";
-import TelaReact from "./componentes/TelaReact";
 import TelaNode from "./componentes/TelaNode";
+import TelaReact from "./componentes/TelaReact";
 
 const MenuNav = createDrawerNavigator();
 
@@ -13,8 +13,8 @@ export default function App() {
     <Navigation.Container>
       <MenuNav.Navigator>
         <MenuNav.Screen name="Java Script" component={ TelaJavaScript } />
-        <MenuNav.Screen name="React" component={ TelaReact } />
         <MenuNav.Screen name="Node" component={ TelaNode } />
+        <MenuNav.Screen name="React" component={ TelaReact } />
       </MenuNav.Navigator>
     </Navigation.Container>
   );
